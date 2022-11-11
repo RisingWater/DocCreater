47604dbb2ca33d62fa5905fd9929b65660b45344
risingwater <4680414@qq.com>
2022-11-11 13:02:16 +0800

    文档生成器初始版本

---
+++ DocCreaterUI/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace DocCreaterUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SelectTemplate = new System.Windows.Forms.Button();
+            this.TemplatePath = new System.Windows.Forms.TextBox();
+            this.ProjectPath = new System.Windows.Forms.TextBox();
+            this.SelectProject = new System.Windows.Forms.Button();
+            this.logBox = new System.Windows.Forms.ListBox();
+            this.GenDoc = new System.Windows.Forms.Button();
+            this.ProjectName = new System.Windows.Forms.TextBox();
+            this.PorjectNameLabel = new System.Windows.Forms.Label();
+            this.TemplateLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.GenDocBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // SelectTemplate
+            // 
+            this.SelectTemplate.Location = new System.Drawing.Point(350, 40);
+            this.SelectTemplate.Name = "SelectTemplate";
+            this.SelectTemplate.Size = new System.Drawing.Size(109, 23);
+            this.SelectTemplate.TabIndex = 0;
+            this.SelectTemplate.Text = "选择模板";
+            this.SelectTemplate.UseVisualStyleBackColor = true;
+            this.SelectTemplate.Click += new System.EventHandler(this.SelectTemplate_Click);
+            // 
+            // TemplatePath
+            // 
+            this.TemplatePath.Enabled = false;
+            this.TemplatePath.Location = new System.Drawing.Point(71, 41);
+            this.TemplatePath.Name = "TemplatePath";
+            this.TemplatePath.Size = new System.Drawing.Size(273, 21);
+            this.TemplatePath.TabIndex = 1;
+            // 
+            // ProjectPath
+            // 
+            this.ProjectPath.Enabled = false;
+            this.ProjectPath.Location = new System.Drawing.Point(71, 70);
+            this.ProjectPath.Name = "ProjectPath";
+            this.ProjectPath.Size = new System.Drawing.Size(273, 21);
+            this.ProjectPath.TabIndex = 2;
+            // 
+            // SelectProject
+            // 
+            this.SelectProject.Location = new System.Drawing.Point(350, 69);
+            this.SelectProject.Name = "SelectProject";
+            this.SelectProject.Size = new System.Drawing.Size(109, 23);
+            this.SelectProject.TabIndex = 3;
+            this.SelectProject.Text = "选择项目目录";
+            this.SelectProject.UseVisualStyleBackColor = true;
+            this.SelectProject.Click += new System.EventHandler(this.SelectProject_Click);
+            // 
+            // logBox
+            // 
+            this.logBox.FormattingEnabled = true;
+            this.logBox.HorizontalScrollbar = true;
+            this.logBox.ItemHeight = 12;
+            this.logBox.Location = new System.Drawing.Point(14, 100);
+            this.logBox.Name = "logBox";
+            this.logBox.Size = new System.Drawing.Size(570, 280);
+            this.logBox.TabIndex = 4;
+            // 
+            // GenDoc
+            // 
+            this.GenDoc.Enabled = false;
+            this.GenDoc.Location = new System.Drawing.Point(465, 12);
+            this.GenDoc.Name = "GenDoc";
+            this.GenDoc.Size = new System.Drawing.Size(119, 80);
+            this.GenDoc.TabIndex = 5;
+            this.GenDoc.Text = "生成文档";
+            this.GenDoc.UseVisualStyleBackColor = true;
+            this.GenDoc.Click += new System.EventHandler(this.GenDoc_Click);
+            // 
+            // ProjectName
+            // 
+            this.ProjectName.Location = new System.Drawing.Point(71, 12);
+            this.ProjectName.Name = "ProjectName";
+            this.ProjectName.Size = new System.Drawing.Size(388, 21);
+            this.ProjectName.TabIndex = 6;
+            this.ProjectName.TextChanged += new System.EventHandler(this.ProjectName_TextChanged);
+            // 
+            // PorjectNameLabel
+            // 
+            this.PorjectNameLabel.AutoSize = true;
+            this.PorjectNameLabel.Location = new System.Drawing.Point(12, 16);
+            this.PorjectNameLabel.Name = "PorjectNameLabel";
+            this.PorjectNameLabel.Size = new System.Drawing.Size(53, 12);
+            this.PorjectNameLabel.TabIndex = 7;
+            this.PorjectNameLabel.Text = "项目名称";
+            // 
+            // TemplateLabel
+            // 
+            this.TemplateLabel.AutoSize = true;
+            this.TemplateLabel.Location = new System.Drawing.Point(12, 45);
+            this.TemplateLabel.Name = "TemplateLabel";
+            this.TemplateLabel.Size = new System.Drawing.Size(53, 12);
+            this.TemplateLabel.TabIndex = 8;
+            this.TemplateLabel.Text = "模板路径";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "项目目录";
+            // 
+            // GenDocBar
+            // 
+            this.GenDocBar.Location = new System.Drawing.Point(14, 384);
+            this.GenDocBar.Name = "GenDocBar";
+            this.GenDocBar.Size = new System.Drawing.Size(570, 23);
+            this.GenDocBar.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 419);
+            this.Controls.Add(this.GenDocBar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TemplateLabel);
+            this.Controls.Add(this.PorjectNameLabel);
+            this.Controls.Add(this.ProjectName);
+            this.Controls.Add(this.GenDoc);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.SelectProject);
+            this.Controls.Add(this.ProjectPath);
+            this.Controls.Add(this.TemplatePath);
+            this.Controls.Add(this.SelectTemplate);
+            this.Name = "Form1";
+            this.Text = "文档生成器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SelectTemplate;
+        private System.Windows.Forms.TextBox TemplatePath;
+        private System.Windows.Forms.TextBox ProjectPath;
+        private System.Windows.Forms.Button SelectProject;
+        private System.Windows.Forms.ListBox logBox;
+        private System.Windows.Forms.Button GenDoc;
+        private System.Windows.Forms.TextBox ProjectName;
+        private System.Windows.Forms.Label PorjectNameLabel;
+        private System.Windows.Forms.Label TemplateLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar GenDocBar;
+    }
+}
+
---
+++ DocCreaterUI/Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Aspose.Words;
+using Aspose.Words.Tables;
+using System.Security.Cryptography;
+using System.IO;
+using System.Collections;
+using System.Threading;
+using Microsoft.Win32;
+
+namespace DocCreaterUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadToReg();
+        }
+
+        private void LoadToReg()
+        {
+            RegistryKey hklm = Registry.CurrentUser;
+            RegistryKey hkConfig = hklm.OpenSubKey(@"SOFTWARE\DocCreater", true);
+            if (hkConfig != null)
+            {
+                object project = hkConfig.GetValue("Project");
+                if (project != null)
+                {
+                    ProjectName.Text = project.ToString();
+                }
+
+                project = hkConfig.GetValue("TemplatePath");
+                if (project != null)
+                {
+                    TemplatePath.Text = project.ToString();
+                }
+
+                project = hkConfig.GetValue("ProjectPath");
+                if (project != null)
+                {
+                    ProjectPath.Text = project.ToString();
+                }
+
+                UpdateGenDocButton();
+            }
+        }
+
+        private void SaveToReg()
+        {
+            RegistryKey hklm = Registry.CurrentUser;
+            RegistryKey hkConfig = hklm.OpenSubKey(@"SOFTWARE\DocCreater", true);
+            if (hkConfig == null)
+            {
+                hkConfig = hklm.CreateSubKey(@"SOFTWARE\DocCreater");
+            }
+
+            hkConfig.SetValue("Project", ProjectName.Text);
+            hkConfig.SetValue("TemplatePath", TemplatePath.Text);
+            hkConfig.SetValue("ProjectPath", ProjectPath.Text);
+
+            hkConfig.Close();
+            hklm.Close();
+        }
+
+        private string Encrypt(string FileName)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            FileStream file = new FileStream(FileName, FileMode.Open);
+            byte[] md5data = md5.ComputeHash(file);
+            md5.Clear();
+            String str = "";
+            for (int i = 0; i < md5data.Length; i++)
+            {
+                str += md5data[i].ToString("x").PadLeft(2, '0');
+            }
+            return str;
+        }
+
+        private void GetFileList(ArrayList filelist, String path)
+        {
+            DirectoryInfo d = new DirectoryInfo(path);
+
+            FileSystemInfo[] fsinfos = d.GetFileSystemInfos();
+            foreach (FileSystemInfo fsinfo in fsinfos)
+            {
+                if (fsinfo is DirectoryInfo)
+                {
+                    if (fsinfo.Name != "depends")
+                    {
+                        GetFileList(filelist, fsinfo.FullName);
+                    }
+                }
+                else
+                {
+                    if (fsinfo.Extension == ".exe"
+                        || fsinfo.Extension == ".dat"
+                        || fsinfo.Extension == ".rpm"
+                        || fsinfo.Extension == ".deb"
+                        || fsinfo.Extension == ".apk"
+                        || fsinfo.Extension == ".pkg")
+                    {
+                        FileInfo fi = new FileInfo(fsinfo.FullName);
+                        filelist.Add(fi);
+                    }
+                }
+            }
+        }
+
+        private Row CreateRow(int columnCount, string[] columnValues, Document doc)
+        {
+            Row r = new Row(doc);
+            for (int i = 0; i < columnCount; i++)
+            {
+                if (columnValues.Length > i)
+                {
+                    Cell cell = CreateCell(columnValues[i], doc);
+                    r.Cells.Add(cell);
+                }
+                else
+                {
+                    Cell cell = CreateCell("", doc);
+                    r.Cells.Add(cell);
+                }
+
+            }
+            return r;
+        }
+
+        private Cell CreateCell(string value, Document doc)
+        {
+            Cell c = new Cell(doc);
+            Paragraph p = new Paragraph(doc);
+            p.AppendChild(new Run(doc, value));
+            c.AppendChild(p);
+            return c;
+        }
+
+        private void UpdateGenDocButton()
+        {
+            if (!string.IsNullOrEmpty(TemplatePath.Text)
+                && !string.IsNullOrEmpty(ProjectPath.Text)
+                && !string.IsNullOrEmpty(ProjectName.Text))
+            {
+                GenDoc.Enabled = true;
+            }
+            else
+            {
+                GenDoc.Enabled = false;
+            }
+        }
+
+        private void Log(String str)
+        {
+            logBox.Items.Add(str);
+            logBox.SelectedIndex = logBox.Items.Count - 1;
+        }
+
+        private void SelectTemplate_Click(object sender, EventArgs e)
+        {
+            var filePath = string.Empty;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "模板文件 |*.doc;*.docx";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog.FileName;
+                TemplatePath.Text = filePath;
+                Log("已选择模板文件：" + TemplatePath.Text);
+                UpdateGenDocButton();
+            }
+        }
+
+        private void SelectProject_Click(object sender, EventArgs e)
+        {
+            var filePath = string.Empty;
+
+            FolderBrowserDialog openFileDialog = new FolderBrowserDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog.SelectedPath;
+                ProjectPath.Text = filePath;
+                Log("已选择模板文件：" + ProjectPath.Text);
+                UpdateGenDocButton();
+            }
+        }
+
+        private void GenDoc_Click(object sender, EventArgs e)
+        {
+            SaveToReg();
+
+            string OutputFileName = "Daas-T-010-01[" + ProjectName.Text + "]版本开发文档[" + DateTime.Now.ToString("yyyymmdd") + "].doc";
+            string OutputFilePath = ProjectPath.Text + "\\doc\\";
+            Log("开始生成文档：" + OutputFileName);
+            Log("文档保存路径：" + OutputFilePath);
+
+            string TemplatePathStr = TemplatePath.Text;
+            string InputPathStr = ProjectPath.Text;
+
+            GenDocBar.Value = 0;
+            GenDocBar.Minimum = 0;
+
+            ProjectName.Enabled = false;
+            SelectTemplate.Enabled = false;
+            SelectProject.Enabled = false;
+            GenDoc.Enabled = false;
+
+            Thread GenDocThread = new Thread(
+                delegate()
+                {
+                    GenDocument(TemplatePathStr, InputPathStr, OutputFilePath + OutputFileName);
+                }
+            );
+
+            GenDocThread.IsBackground = true;
+            GenDocThread.Start();
+        }
+
+        private void GenDocument(string TemplatePathStr, string InputPathStr, string OutputFilePath)
+        {
+            Document doc = new Document(TemplatePathStr);
+            DocumentBuilder mainDocBuilder = new DocumentBuilder(doc);
+
+            ArrayList filelist = new ArrayList();
+            GetFileList(filelist, InputPathStr + "/server");
+            GetFileList(filelist, InputPathStr + "/client");
+
+            GenDocBar.Maximum = filelist.Count;
+
+            mainDocBuilder.MoveToBookmark("FILELIST", true, true);
+
+            mainDocBuilder.StartTable();
+
+            mainDocBuilder.InsertCell();
+            mainDocBuilder.Write("文件名");
+
+            mainDocBuilder.InsertCell();
+            mainDocBuilder.Write("文件大小");
+
+            mainDocBuilder.InsertCell();
+            mainDocBuilder.Write("校验和/MD5值");
+
+            mainDocBuilder.InsertCell();
+            mainDocBuilder.Write("使用说明");
+
+            mainDocBuilder.EndRow();
+
+            foreach (FileInfo fi in filelist)
+            {
+                mainDocBuilder.InsertCell();
+                mainDocBuilder.Write(fi.Name);
+
+                mainDocBuilder.InsertCell();
+                mainDocBuilder.Write(fi.Length.ToString());
+
+                string md5 = Encrypt(fi.FullName);
+
+                mainDocBuilder.InsertCell();
+                mainDocBuilder.Write(md5);
+
+                mainDocBuilder.InsertCell();
+                mainDocBuilder.Write("安装使用");
+
+                mainDocBuilder.EndRow();
+
+                Log("文件: " + fi.FullName + " 大小：" + fi.Length.ToString() + " MD5: " + md5);
+
+                GenDocBar.Value++;
+            }
+
+            Table table = mainDocBuilder.EndTable();
+            table.AutoFit(AutoFitBehavior.AutoFitToWindow);
+
+            doc.Save(OutputFilePath);
+
+            Log("成功生成文档：" + OutputFilePath);
+
+            ProjectName.Enabled = true;
+            SelectTemplate.Enabled = true;
+            SelectProject.Enabled = true;
+            GenDoc.Enabled = true;
+        }
+
+        private void ProjectName_TextChanged(object sender, EventArgs e)
+        {
+            UpdateGenDocButton();
+        }
+    }
+}
